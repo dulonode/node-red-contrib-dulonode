8487a7f38db6faa242b5d7d8206075c4ee867773
Teodor Ivanov <teodor.ivanov@dulonode.com>
2025-01-13 16:25:52 +0200

    Client ID update

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const package = require('./package.json');
 const settings = {
   version: package.version,
   region: 'us-east-1',
-  clientId: '2939p25a4thbgs3a8rk5nud8mr',
+  clientId: '63617agok8d3vt7sbhph4mvkfc',
   apiURL: 'https://api.dulonode.com'
 };
 
